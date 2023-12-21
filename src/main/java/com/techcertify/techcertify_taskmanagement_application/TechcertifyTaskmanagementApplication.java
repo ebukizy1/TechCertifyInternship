d85bc3e856299c4e4c97e1b90367a732a7fd8e54
ebukizy1 <ebukizy1@gmail.com>
2023-12-21 15:09:01 +0100

    first push

---
+++ src/main/java/com/techcertify/techcertify_taskmanagement_application/TechcertifyTaskmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.techcertify.techcertify_taskmanagement_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechcertifyTaskmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechcertifyTaskmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techcertify/techcertify_taskmanagement_application/TechcertifyTaskmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techcertify.techcertify_taskmanagement_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechcertifyTaskmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
